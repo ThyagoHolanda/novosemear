c752df6bed6c36c9ada1ef115462fd992c85ffbb
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2023-07-19 00:05:56 -0300

    Melhora no calculo

--- progressbar.js
+++ progressbar.js
@@ -1,10 +1,29 @@
-const anne = [25];
-const renan = [25, 25, 25];
-const mayara = [25, 25];
-const thyago = [25];
-const hely = [25];
+const anne = [25, 0, 0, 0];
+const renan = [25, 25, 25, 0];
+const mayara = [25, 25, 25 ,0];
+const thyago = [25, 0, 0, 0];
+const hely = [25, 0, 0, 0];
+var soma = 0;
 
-var amountRaised = (anne.length + renan.length + mayara.length + thyago.length + hely.length) * 25;
+for (i = 0; i < 12; i++) {
+  if (i<anne.length) {
+      soma += Number(anne[i]);
+  }
+  if (i<renan.length) {
+      soma += Number(renan[i]);
+  }
+  if (i<mayara.length) {
+      soma += Number(mayara[i]);
+  }
+  if (i<thyago.length) {
+      soma += Number(thyago[i]);
+  }
+  if (i<hely.length) {
+      soma += Number(hely[i]);
+  }
+}
+
+var amountRaised = soma;
 var goal = (25 * 11) * 25;
 
 function updateProgress() {
