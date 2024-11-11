d4bbef9c1fb0cb1aef994d23158e6a91260634c2
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2024-11-10 21:18:22 -0300

    Atualizado para 2025

--- script/progressbar.js
+++ script/progressbar.js
@@ -1,38 +1,33 @@
 const pag = [
-    { nome: "Anny", mar: 25, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Renan", mar: 25, abr: 25, mai: 25, jun: 25, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Mayara", mar: 25, abr: 25, mai: 25, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Thyago", mar: 25, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Helyziane", mar: 25, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Matheus", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Samuel", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Julio", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Gabriel", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Junior", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Igor", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Jefferson", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Sofia", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Cena", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Leticia Soares", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Leticia Ellen", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Vitoria Mariana", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Vitoria", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Geovana", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Emily", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Eriki", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Agatha", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Bianca", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Clara", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Mikael", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Rodrigo", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
-    { nome: "Yasmin", mar: 0, abr: 0, mai: 0, jun: 0, jul: 0, ago: 0, set: 0, out: 0, nov: 0, dez: 0, jan: 0 },
+    { nome: "Renan", mar: 0},
+    { nome: "Mayara", mar: 402},
+    { nome: "Thyago", mar: 33},
+    { nome: "Helyziane", mar: 219},
+    { nome: "Samuel", mar: 0},
+    { nome: "Gabriel", mar: 0},
+    { nome: "Junior", mar: 72},
+    { nome: "Igor", mar: 0},
+    { nome: "Sofia", mar: 154+9},
+    { nome: "Cena", mar: 0},
+    { nome: "Leticia Soares", mar:0},
+    { nome: "Vitoria Mariana", mar:0},
+    { nome: "Geovana", mar: 213},
+    { nome: "Emily", mar: 0},
+    { nome: "Eriki", mar: 0},
+    { nome: "Rodrigo", mar: 0},
+    { nome: "Isabele", mar: 50},
+    { nome: "Castnia", mar: 0},
+    { nome: "Adriano", mar: 0},
+    { nome: "Ester", mar: 18},
+    { nome: "Gregorio", mar: 36},
+    { nome: "Feijoada", mar: 1000},
 ]
 
 // Ordena o array em ordem alfabética pelo nome das pessoas
 pag.sort((a, b) => a.nome.localeCompare(b.nome));
 
 let amountRaised = totalPag();
-let goal = (25 * 11) * pag.length;
+let goal = 400 * (pag.length-1);
 
 function totalPag() {
   let soma = 0;
@@ -85,7 +80,7 @@ function totalMes() {
 
 function updateProgress() {
   let countdown = document.querySelector(".countdown");
-  let targetDate = new Date("02/09/2024");
+  let targetDate = new Date("03/28/2025");
   let daysElement = document.getElementById("days");
   let hoursElement = document.getElementById("hours");
   let minutesElement = document.getElementById("minutes");
